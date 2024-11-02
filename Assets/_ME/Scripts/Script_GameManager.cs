ac20ddd140a8b4f467ff9a38301e5bec8d132c5c
Boneco Sinforoso <icmgc98@gmail.com>
2024-11-02 19:09:21 -0300

    setup inicial da cena

--- Assets/_ME/Scripts/Script_GameManager.cs
+++ Assets/_ME/Scripts/Script_GameManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Script_GameManager : MonoBehaviour
 {
+    public static Script_GameManager instance;
 
+    [SerializeField] float cam_moveSpeed;    
+    [SerializeField] int player_victimQuant;
+    [SerializeField] int player_money;
+
+    [Header("Elementos do canvas")]
+    [SerializeField] TextMeshProUGUI txt_fps;
+    [SerializeField] Button btn_punch;
+    [SerializeField] Button btn_throw;
+
+    Transform t_cam;
+    Transform t_player;
 
     void Start()
     {
-        
+        instance = this;
+
+        t_cam = Camera.main.transform.parent;
+        t_player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Area_Punch_Set(false);
+        Area_Throw_Set(false);
     }
 
     void Update()
     {
-        
+        txt_fps.text = (1f / Time.deltaTime).ToString();
+
+        t_cam.position = Vector3.Lerp(t_cam.position, t_player.position, cam_moveSpeed * Time.deltaTime);
+    }
+
+    public void Area_Punch_Set(bool _value)
+    {
+        btn_punch.interactable = _value;
+    }
+
+    public void Area_Throw_Set(bool _value)
+    {
+        if (_value && player_victimQuant == 0) return;
+        btn_throw.interactable = _value;
+    }
+
+    public void Txt_Money_Set()
+    {
+        txt_fps.text = player_money.ToString();
     }
 }
---
+++ Assets/_ME/Scripts/Script_Player.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class Script_Player : MonoBehaviour
+{
+    [SerializeField] RectTransform rect_joy_front;
+    [SerializeField] RectTransform rect_joy_back;
+
+    [SerializeField] float moveSpeed;
+
+    [SerializeField] Transform t_move;
+    Animator animator;
+    Rigidbody rb;
+
+    void Start()
+    {
+        animator = transform.GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        bool _joyMoved = Vector2.Distance(rect_joy_back.position, rect_joy_front.position) > 0.1f;
+
+        animator.SetBool("_run", _joyMoved);
+
+        if (_joyMoved)
+        {
+            t_move.eulerAngles = new Vector3(0, -Script_Util.Direction2D_Get(rect_joy_back.position, rect_joy_front.position) + 90, 0);
+
+            rb.velocity = t_move.forward * moveSpeed;
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    public void Action_Punch()
+    {
+
+    }
+
+    public void Action_Throw()
+    {
+
+    }
+
+    #region Colisao
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+
+        if (other.CompareTag("Area_Punch"))
+        {
+            Script_GameManager.instance.Area_Punch_Set(true);
+        }
+        if (other.CompareTag("Area_Throw"))
+        {
+            Script_GameManager.instance.Area_Throw_Set(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Area_Punch"))
+        {
+            Script_GameManager.instance.Area_Punch_Set(false);
+        }
+        if (other.CompareTag("Area_Throw"))
+        {
+            Script_GameManager.instance.Area_Throw_Set(false);
+        }
+    }
+    #endregion
+}
